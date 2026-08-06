bc56b0e7d0e8a576c572a6ae40e7419f77beb08a
rolpppp <rlgarces@up.edu.ph>
2026-08-06 21:15:20 +0800

    chore: add Supabase, dotenv, and HTTP dependencies and bump Android/iOS platform   config to support them

--- android/app/src/main/kotlin/dev/rolfgarces/salin/MainActivity.kt
+++ android/app/src/main/kotlin/dev/rolfgarces/salin/MainActivity.kt
@@ -1,5 +1,11 @@
 package dev.rolfgarces.salin
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
